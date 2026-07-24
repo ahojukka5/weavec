d7e9c49034e8b2d3b066e4732e4569ad6a6a750c
Jukka Aho <ahojukka5@gmail.com>
2026-07-25 02:59:08 +0300

    build: define portability features before support includes

--- runtime/portable.c
+++ runtime/portable.c
@@ -4,6 +4,13 @@
 // responsibilities are split so release packaging can expose only the program
 // runtime while keeping compiler support and the build driver private.
 
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 700
+#endif
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+
 #include "compiler_support.c"
 #include "program_runtime.c"
 #include "build_driver.c"
